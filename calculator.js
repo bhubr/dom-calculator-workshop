115245849059b59ed384c85bb513734bdf0b749d
Benoît Hubert <benoit.hubert@wildcodeschool.com>
2020-09-28 12:16:13 +0200

    Add error handling

--- calculator.js
+++ calculator.js
@@ -13,8 +13,20 @@ calculateBtn.addEventListener(
     // 2. convertir les valeurs des opérandes
     // 3. effectuer l'opération en fonction de l'opérateur
 
+    if (operand1.value === '' || operand2.value === '') {
+      resultDiv.innerText = 'Veuillez saisir deux valeurs';
+      return;
+    }
     const op1int = parseInt(operand1.value);
     const op2int = parseInt(operand2.value);
+    if (Number.isNaN(op1int) || Number.isNaN(op2int)) {
+      resultDiv.innerText = 'Veuillez saisir des nombres valides';
+      return;
+    }
+    if (operator.value === '/' && op2int === 0) {
+      resultDiv.innerText = 'Impossible de diviser par zéro !';
+      return;
+    }
     let result;
 
     switch (operator.value) {
